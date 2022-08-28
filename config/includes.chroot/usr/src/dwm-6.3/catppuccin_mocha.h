6a48bdfe3cf658a27a173fe6ca27df38fc42bf15
Roman <imspeedwagon@gmail.com>
2022-08-29 01:01:53 +0300

    Build custom DWM and dmenu in chroot stage
    Include configs and patches for DWM and dmenu.
    
    Add packages from the build requirements in the list.
    
    Add hooks for building DWM and dmenu in the chroot.

---
+++ config/includes.chroot/usr/src/dwm-6.3/catppuccin_mocha.h
@@ -0,0 +1,35 @@
+/* Background colors */
+static const char col_base[] = "#1e1e2e";
+
+static const char col_crust[] = "#11111b";
+static const char col_mantle[] = "#181825";
+
+static const char col_surface0[] = "#313244";
+static const char col_surface1[] = "#45475a";
+static const char col_surface2[] = "#585b70";
+
+static const char col_overlay0[] = "#6c7086";
+static const char col_overlay1[] = "#7f849c";
+static const char col_overlay2[] = "#9399b2";
+
+/* Typography */
+static const char col_text[] = "#cdd6f4";
+
+static const char col_subtext0[] = "#a6adc8";
+static const char col_subtext1[] = "#bac2de";
+
+/* Highlight colors */
+static const char col_rosewater[] = "#f5e0dc";
+static const char col_flamingo[] = "#f2cdcd";
+static const char col_pink[] = "#f5c2e7";
+static const char col_mauve[] = "#cba6f7";
+static const char col_red[] = "#f38ba8";
+static const char col_maroon[] = "#eba0ac";
+static const char col_peach[] = "#fab387";
+static const char col_yellow[] = "#f9e2af";
+static const char col_green[] = "#a6e3a1";
+static const char col_teal[] = "#94e2d5";
+static const char col_sky[] = "#89dceb";
+static const char col_sapphire[] = "#74c7ec";
+static const char col_blue[] = "#89b4fa";
+static const char col_lavender[] = "#b4befe";
